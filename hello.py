1a69325f36bd0b3a91aad3247bcbc1c3cf22788f
Shree Ram <mekashreeram@gmail.com>
2023-12-10 23:26:30 +0530

    Create Basic Interface to upload and download videos

--- hello.py
+++ hello.py
@@ -1,14 +1,38 @@
-from flask import Flask, render_template, url_for, request, send_file
+from flask import Flask, render_template, url_for, request, send_file, Response
 from fileinput import filename
+from io import BytesIO
+import io
+
 app = Flask(__name__)
+
+video_data = None
+filename = None
 
 @app.route('/', methods = ['POST', 'GET'])
 def index():
+  global video_data
   if request.method == "POST":
+    global f, filename
     f = request.files['file']
-    return send_file(f, mimetype='video/mov')
+    filename = f.filename
+    video_data = BytesIO(f.read())
+    video_data.seek(0)
+    return render_template('video-preview.html')
   else:
     return render_template('index.html')
+
+@app.route('/video_stream')
+def video_stream():
+  global video_data
+  if video_data is not None:
+    print("Video data length:", len(video_data.getvalue()))
+    return Response(video_data.getvalue(), mimetype='video/*', content_type='video/*; charset=utf-8')
+  return "No video data available."
+
+@app.route('/download', methods = ['POST', 'GET'])
+def download():
+  global filename, video_data
+  return send_file(video_data, as_attachment=True, download_name="translated_" + f.filename)
 
 if __name__ == '__main__':
   app.run(debug = True, port=3000)
---
+++ static/script.js
@@ -0,0 +1,54 @@
+const dropArea = document.getElementById("drop-area");
+const inputFile = document.getElementById("input-file");
+const imgView = document.getElementById("img-view");
+const upload = document.getElementById("upload");
+const lang = document.getElementById("lang");
+const form = document.getElementById("form");
+
+inputFile.addEventListener("change", uploadImage);
+
+function uploadImage() {
+  const extensions = new Set([
+    "mp4",
+    "mov",
+    "mkv",
+    "avi",
+    "wmv",
+    "flv",
+    "webm",
+    "avchd",
+  ]);
+  const fileName = inputFile.files[0].name;
+  let imgLink = URL.createObjectURL(inputFile.files[0]);
+  const ext = fileName.split(".").pop();
+  if (!extensions.has(ext.toLowerCase())) {
+    alert("Invalid File Format");
+    return;
+  }
+  imgView.innerHTML = `<video controls><source src=${imgLink}></video>`;
+  imgView.style.border = 0;
+}
+
+dropArea.addEventListener("dragover", (e) => {
+  e.preventDefault();
+});
+
+dropArea.addEventListener("drop", (e) => {
+  e.preventDefault();
+  inputFile.files = e.dataTransfer.files;
+  uploadImage();
+});
+
+upload.addEventListener("click", (e) => {
+  if (!inputFile.files[0]) {
+    alert("Please upload or select a video first");
+    e.preventDefault();
+    return;
+  }
+  if (lang.value === "hide") {
+    alert("Please select a language");
+    e.preventDefault();
+  } else {
+    form.submit();
+  }
+});
