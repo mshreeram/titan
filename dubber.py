4ffb19928740458dd5c9c15634b7cbb08d686d8e
Shree Ram <mekashreeram@gmail.com>
2023-12-14 10:14:35 +0530

    Create dubberv2 using openai llm

--- dubber.py
+++ dubber.py
@@ -1,17 +1,3 @@
-# Copyright 2021 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from pydub import AudioSegment
 from google.cloud import speech_v1p1beta1 as speech
 from google.cloud import texttospeech
@@ -476,6 +462,7 @@ def dub(
             for sentence in sentences:
                 sentence[lang] = translate_text(
                     sentence[srcLang], lang, srcLang)
+                print(f"text={sentence[lang]}\n")
 
         # Write the translations to json
         fn = os.path.join(outputDir, baseName + ".json")
